a7f60d5be770b74be846b63d3cd2314c05dc9330
MichalK <95657053+michalK00@users.noreply.github.com>
2023-03-19 17:58:32 +0100

    done config

---
+++ src/main/java/com/sampla/samplaapi/SamplaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sampla.samplaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sampla/samplaapi/SamplaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampla.samplaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
